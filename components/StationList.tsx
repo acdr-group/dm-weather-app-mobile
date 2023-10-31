183abff2ae3870ac3c8b813af7438efe38fd390a
ramses1998 <ramsestetang2017@outlook.com>
2023-10-31 22:36:44 +0100

    Added cards to the analysis screen

--- components/StationList.tsx
+++ components/StationList.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {COLORS, SIZES} from
-        "../constatnts";
 import {Station} from "../models/station";
 import {useRouter} from "expo-router";
+import GenericList, {ListItem} from "./shared/GenericList";
 
 type Props = {
     stations: Station[]
@@ -11,88 +9,23 @@ type Props = {
 
 const StationList: React.FC<Props> = (props: Props) => {
 
-    if (props.stations.length === 0) {
-        return <Text>Keine Stationen gefunden</Text>
-    }
-
-    return (
-        <View style={styles.stationListContainer}>
-            {props.stations.map((station, index, array) =>
-                <StationItem
-                    {...station}
-                    key={station.name}
-                    showBottomDivider={index !== array.length - 1}
-                />
-            )}
-        </View>
-    )
-}
-
-type PropsStationItem = Station & {
-    showBottomDivider?: boolean
-}
-
-const StationItem: React.FC<PropsStationItem> = (props: PropsStationItem) => {
-
     const router = useRouter()
-    const forwardIcon = require("../assets/icons/arrow_forward_ios.png")
 
-    const handlePress = () => {
-        router.push(`/stations/${props.id}`)
-    }
+    const stationsToShow: ListItem[] = props.stations.map(s => {
+        return {
+            title: s.label,
+            subtitle: s.country,
+            icon: require("../assets/icons/arrow_forward_ios.png"),
+            onPress: () => router.push(`/stations/${s.id}`)
+        }
+    })
 
     return (
-        <TouchableOpacity
-            onPress={handlePress}
-            style={[
-            styles.stationItemContainer,
-            (props.showBottomDivider ? styles.bottomBorder : null)
-        ]}>
-            <View>
-                <Text style={styles.stationItemName}>
-                    {props.label}
-                </Text>
-                <Text style={styles.stationItemDescription}>
-                    {props.country}
-                </Text>
-            </View>
-            <Image source={forwardIcon} style={styles.icon}/>
-        </TouchableOpacity>
+        <GenericList
+            listData={stationsToShow}
+            emptyListMessage={"Keine Stationen gefunden"}
+        />
     )
 }
-
-
-const styles = StyleSheet.create({
-    stationListContainer: {
-        flex: 1,
-        borderColor: COLORS.gray2,
-        borderWidth: 1,
-        borderRadius: SIZES.small,
-        paddingLeft: SIZES.medium,
-        paddingVertical: 3,
-    },
-    stationItemContainer: {
-        paddingVertical: 10,
-        paddingRight: 10,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between"
-    },
-    bottomBorder: {
-        borderBottomWidth: 1,
-        borderBottomColor: COLORS.gray2,
-    },
-    stationItemName: {
-        fontSize: 18,
-    },
-    stationItemDescription: {
-        fontSize: 14,
-        color: COLORS.gray,
-    },
-    icon: {
-        height: 14,
-        width: 14,
-    }
-})
 
 export default StationList
---
+++ components/shared/GenericList.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {COLORS, SIZES} from "../../constatnts";
+import {FONT_SIZE, GAPS} from "../../constatnts/theme";
+
+export interface ListItem {
+    title: string
+    subtitle?: string
+    icon?: any
+    leftSideText?: string
+    onPress?: () => void
+}
+type Props = {
+    listData: ListItem[]
+    emptyListMessage?: string
+}
+
+const GenericList: React.FC<Props> = (props: Props) => {
+
+    if (props.listData.length === 0) {
+        return <Text>{props.emptyListMessage ?? "Keine Daten gefunden!"}</Text>
+    }
+
+    return (
+        <View style={styles.listContainer}>
+            {props.listData.map((data, index, array) =>
+                <ListItem
+                    {...data}
+                    key={`${data.title} ${data.subtitle}`}
+                    showBottomDivider={index !== array.length - 1}
+                />
+            )}
+        </View>
+    )
+}
+
+type PropsListItem = ListItem & {
+    showBottomDivider?: boolean
+}
+
+const ListItem: React.FC<PropsListItem> = (props: PropsListItem) => {
+    return (
+        <TouchableOpacity
+            onPress={props.onPress}
+            style={[
+                styles.listItemContainer,
+                (props.showBottomDivider ? styles.bottomBorder : null)
+            ]}>
+            <View>
+                <Text style={styles.title}>
+                    {props.title}
+                </Text>
+                {props.subtitle ?
+                    <Text style={styles.subtitle}>
+                        {props.subtitle}
+                    </Text>
+                    : null
+                }
+            </View>
+            <View style={styles.leftSideOfList}>
+                {props.leftSideText ?
+                    <Text style={styles.leftSideText}>{props.leftSideText}</Text> : null
+                }
+                {props.icon ?
+                    <Image source={props.icon} style={styles.icon}/> : null
+                }
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    listContainer: {
+        flex: 1,
+        borderColor: COLORS.gray2,
+        borderWidth: 1,
+        borderRadius: SIZES.small,
+        paddingLeft: SIZES.medium,
+        paddingVertical: 3,
+    },
+    listItemContainer: {
+        paddingVertical: SIZES.xxSmall,
+        paddingRight: SIZES.xxSmall,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    bottomBorder: {
+        borderBottomWidth: 1,
+        borderBottomColor: COLORS.gray2,
+    },
+    title: {
+        fontSize: FONT_SIZE.medium,
+    },
+    subtitle: {
+        fontSize: FONT_SIZE.xSmall,
+        color: COLORS.gray,
+    },
+    leftSideOfList: {
+      flexDirection: "row",
+      gap: GAPS.gap2,
+    },
+    leftSideText: {
+        fontSize: FONT_SIZE.medium,
+        color: COLORS.gray,
+    },
+    icon: {
+        height: SIZES.small,
+        width: SIZES.small,
+    }
+})
+
+export default GenericList
